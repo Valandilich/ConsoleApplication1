91fcb8cc8e22249e4df39d53af0581362f1aa1d9
Jiazhou <Jiazhou@DESKTOP-ISSDV06>
2018-03-12 20:13:34 +0800

    精细化快排

--- ConsoleApplication1/quickSort.h
+++ ConsoleApplication1/quickSort.h
@@ -1,32 +1,47 @@
 #pragma once
 #include<vector>
 using namespace std;
-void quickSort(vector<int> &inPut,int head,int tail)
+
+bool MySwap(vector<int> &inPut, int a, int b)
 {
-	if (tail-head<1)
-		return;
+	if (a > inPut.size() - 1 || b > inPut.size() - 1)
+		return false;
+	int t = inPut[a];
+	inPut[a] = inPut[b];
+	inPut[b] = t;
+}
+int Devide(vector<int> &inPut, int head, int tail)
+{
 	int mid = inPut.at(head);
 	int m_head = head;
 	int m_tail = tail;
 	while (m_head < m_tail)
 	{
-		while (inPut.at(m_head) <=mid&&m_head < m_tail)
+		while (m_head <= m_tail && inPut.at(m_head) <= mid)
 			m_head++;
-		while (inPut.at(m_tail) > mid &&m_head < m_tail)
+		while (m_head < m_tail && inPut.at(m_tail) > mid)
 			m_tail--;
 		if (!(m_head < m_tail))
 			break;
-		int t = inPut[m_head];
-		inPut[m_head] = inPut[m_tail];
-		inPut[m_tail] = t;
-		m_head++;
-		m_tail--;
+// 		if(inPut.at(m_head)>inPut.at(m_tail))
+// 		{
+			MySwap(inPut, m_head, m_tail);
+			m_head++;
+			m_tail--;
+//		}
 	}
-	if (inPut[m_head] > mid)
-		m_head--;
-	int t = inPut[m_head];
-	inPut[m_head] = inPut[head];
-	inPut[head] = t;
+	if (m_head>tail || mid < inPut.at(m_head))
+		return m_head - 1;
+	else
+		return m_head;
+}
+
+void quickSort(vector<int> &inPut,int head,int tail)
+{
+	if (tail-head<1)
+		return;
+	int m_head = Devide(inPut, head, tail);
+	MySwap(inPut, m_head, head);
 	quickSort(inPut, head, m_head-1);
 	quickSort(inPut, m_head+1, tail);
 	return;
